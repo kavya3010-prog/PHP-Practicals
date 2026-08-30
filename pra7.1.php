28de8b9cad62c3aa42304ff2f84edb97c59f4c26
Kavya Patel <kavyapatel30107@gmail.com>
2026-08-30 16:52:20 +0530

    Add files via upload

---
+++ pra7.1.php
@@ -0,0 +1,38 @@
+<?php
+
+// Current date
+echo "Current Date: " . date("d-m-Y");
+echo "<br>";
+
+// Current time
+echo "Current Time: " . date("H:i:s");
+echo "<br>";
+
+// Day
+echo "Day: " . date("l");
+echo "<br>";
+
+// Month
+echo "Month: " . date("F");
+echo "<br>";
+
+// Year
+echo "Year: " . date("Y");
+echo "<br>";
+
+// Date in different format
+echo "Date: " . date("d/m/Y");
+echo "<br>";
+
+// Time in 12-hour format
+echo "Time: " . date("h:i:s A");
+echo "<br>";
+
+// Current timestamp
+echo "Timestamp: " . time();
+echo "<br>";
+
+// Current date and time
+echo "Date and Time: " . date("d-m-Y H:i:s");
+
+?>
