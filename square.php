8f638d0cfc5e13ff44ebc85671ac4b3f04d12c06
Kavya Patel <kavyapatel30107@gmail.com>
2026-09-04 09:58:54 +0530

    Add files via upload

---
+++ square.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Squares of Numbers</title>
+</head>
+<body>
+
+<h2>Squares of Numbers (1–12)</h2>
+
+<?php
+for($i=1;$i<=12;$i++)
+{
+    echo $i." * ".$i." = ".($i*$i)."<br>";
+}
+?>
+
+</body>
+</html>
