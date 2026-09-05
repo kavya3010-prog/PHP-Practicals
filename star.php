48af4288ec9cfaf4b25919d8900c5757e9187a97
Kavya Patel <kavyapatel30107@gmail.com>
2026-09-05 11:22:35 +0530

    Add files via upload

---
+++ star.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Star Pattern</title>
+</head>
+<body>
+
+<h2>Star Pattern</h2>
+
+<?php
+for($i=1;$i<=5;$i++)
+{
+    for($j=1;$j<=$i;$j++)
+    {
+        echo "* ";
+    }
+    echo "<br>";
+}
+?>
+
+</body>
+</html>
