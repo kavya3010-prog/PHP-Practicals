2fe13a9737b54b6bc67ed287248a7f9142f9a0df
Kavya Patel <kavyapatel30107@gmail.com>
2026-09-12 15:41:29 +0530

    Add files via upload

---
+++ numberpattern.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Number Pattern</title>
+</head>
+<body>
+
+<h2>Number Pattern</h2>
+
+<?php
+for($i=1;$i<=5;$i++)
+{
+    for($j=1;$j<=$i;$j++)
+    {
+        echo $j." ";
+    }
+    echo "<br>";
+}
+?>
+
+</body>
+</html>
