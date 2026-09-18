038a428be764f805bec98ed0bd5723afe9890eb6
Kavya Patel <kavyapatel30107@gmail.com>
2026-09-18 19:08:16 +0530

    Add files via upload

---
+++ form1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Form 1</title>
+</head>
+<body>
+
+<form action="form2.php" method="post">
+    Name:
+    <input type="text" name="name"><br><br>
+
+    Email:
+    <input type="email" name="email"><br><br>
+
+    <input type="submit" value="Next">
+</form>
+
+</body>
+</html>
